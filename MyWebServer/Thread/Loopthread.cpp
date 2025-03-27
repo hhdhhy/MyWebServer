4fab84dd3080804516cc32c9d03bafe5abfd4ac3
hhdhhy <1215157605@qq.com>
2025-03-27 14:24:25 +0000

    modified:   MyWebServer/Thread/Loopthread.cpp 	modified:   MyWebServer/Thread/Loopthread.h

--- MyWebServer/Thread/Loopthread.cpp
+++ MyWebServer/Thread/Loopthread.cpp
@@ -1,1 +1,37 @@
 #include<Loopthread.h>
+
+Loopthread::Loopthread()
+:thread_(std::make_shared<std::thread>([this](){run_loop();}))
+{
+
+}
+
+Loopthread::~Loopthread()
+{
+    if(loop_)
+    {
+        loop_->stop();
+        thread_->join();
+    }
+}
+
+std::shared_ptr<Loop> Loopthread::get_loop()
+{
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        cond_.wait(lock, [this](){return loop_.get() != nullptr;});
+    }
+    return loop_;
+}
+
+void Loopthread::run_loop()
+{
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        loop_=std::make_shared<Loop>();
+        cond_.notify_all();
+    }
+    loop_->run();
+    std::unique_lock<std::mutex> lock(mutex_);
+    loop_.reset();
+}
--- MyWebServer/Thread/Loopthread.h
+++ MyWebServer/Thread/Loopthread.h
@@ -1,3 +1,24 @@
 #pragma once 
 #include "Loop.h"
+#include <condition_variable>
+
+class Loopthread
+{
+
+public:
+    Loopthread();
+    ~Loopthread();
+
+    void run_loop();// 线程函数启动loop
+    
+    std::shared_ptr<Loop> get_loop();
+private:
+    
+
+    std::shared_ptr<std::thread> thread_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    std::shared_ptr<Loop> loop_;//在临界区中得加锁
+};
+
 
