02e047f4feaae987e715b50f5a8d590f3c94723f
hhdhhy <1215157605@qq.com>
2025-03-29 07:48:05 +0000

    modified:   MyWebServer/Tcpserver/Tcpserver.cpp 	modified:   MyWebServer/Tcpserver/Tcpserver.h

---
+++ MyWebServer/Tcpserver/Tcpserver.cpp
@@ -0,0 +1,16 @@
+#include "Tcpserver.h"
+Tcpserver::Tcpserver(/* args */)
+{
+}
+
+Tcpserver::~Tcpserver()
+{
+}
+
+void Tcpserver::set_callback_connect(callback_connect callback)
+{
+}
+
+void Tcpserver::set_callback_message(callback_message callback)
+{
+}
--- MyWebServer/Tcpserver/Tcpserver.h
+++ MyWebServer/Tcpserver/Tcpserver.h
@@ -1,1 +1,28 @@
-#include<
+#pragma once
+
+#include "Acceptor.h"
+#include "Tcpconnection.h"
+#include <map>
+class Tcpserver
+{
+public:
+    typedef std::function<void(int,sockaddr_in)> callback_connect;
+    typedef std::function<void(int,sockaddr_in)> callback_message;
+
+    Tcpserver(/* args */);
+    ~Tcpserver();
+    
+    void set_callback_connect(callback_connect callback);
+    void set_callback_message(callback_message callback);
+
+private:
+    
+    Acceptor acceptor_;
+    std::shared_ptr<Loop> loop_;
+
+    std::map<int,std::shared_ptr<Tcpconnection>> connections_;
+    int next_connection_id_;
+    callback_connect callback_connect_;
+    callback_message callback_message_;
+};
+
